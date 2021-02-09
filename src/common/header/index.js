4e929069207cc535df0fc7bce3af00d3b6d83e28
lceihen <2750418124@qq.com>
2021-02-09 15:31:54 +0800

    7-5完成

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,32 +1,67 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group'
 import {
 	HeaderWrapper,
 	Logo,
 	Nav,
 	NavItem,
+	SearchWrapper,
 	NavSearch,
 	Addition,
 	Button
 } from './style';
 
 class Header extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			focused: false
+		}
+		this.handleInputFocus = this.handleInputFocus.bind(this)
+		this.handleInputBlur = this.handleInputBlur.bind(this)
+	}
 	render() {
 		return (
 			<HeaderWrapper>
-				<Logo/>
+				<Logo />
 				<Nav>
 					<NavItem className='left active'>首页</NavItem>
 					<NavItem className='left'>下载App</NavItem>
 					<NavItem className='right'>登陆</NavItem>
-					<NavItem className='right'>Aa</NavItem>
-					<NavSearch></NavSearch>
+					<NavItem className='right'>
+						<i className="iconfont">&#xe636;</i>
+					</NavItem>
+					<SearchWrapper>
+						<CSSTransition
+							in={this.state.focused}
+							timeout={200}
+							className="slide"
+						>
+							<NavSearch className={this.state.focused ? "focused" : ""}
+								onClick={this.handleInputFocus} onBlur={this.handleInputBlur}></NavSearch>
+						</CSSTransition>
+						<i className={this.state.focused ? "focused iconfont" : "iconfont"}>&#xe614;</i>
+					</SearchWrapper>
 				</Nav>
 				<Addition>
-					<Button className='writting'>写文章</Button>
+					<Button className='writting'>
+						<i className="iconfont">&#xe615;</i>
+						写文章
+					</Button>
 					<Button className='reg'>注册</Button>
 				</Addition>
 			</HeaderWrapper>
 		)
+	}
+	handleInputFocus() {
+		this.setState({
+			focused: true
+		})
+	}
+	handleInputBlur() {
+		this.setState({
+			focused: false
+		})
 	}
 }
 
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -45,12 +45,42 @@ export const NavItem = styled.div`
 	}
 `;
 
+export const SearchWrapper = styled.div`
+	position: relative;
+	float: left;
+	.slide-enter{
+		transition:all .2s 
+	}
+	.slide-enter-active{
+		width:240px
+	}
+	.slide-exit{
+		transition:all .2s 
+	}
+	.slide-enter-exit{
+		width:160px
+	}
+	.iconfont {
+		position: absolute;
+		right: 5px;
+		bottom: 5px;
+		width: 30px;
+		line-height: 30px;
+		border-radius: 15px;
+		text-align: center;
+		&.focused{
+			background:#777;
+			color:#fff;
+		}
+	}
+`;
+
 export const NavSearch = styled.input.attrs({
 	placeholder: '搜索'
 })`
-	width: 160p;
+	width: 160px;
 	height: 38px;
-	padding: 0 20px;
+	padding: 0 35px 0 20px;
 	margin-top: 9px;
 	margin-left: 20px;
 	box-sizing: border-box;
@@ -59,8 +89,12 @@ export const NavSearch = styled.input.attrs({
 	border-radius: 19px;
 	background: #eee;
 	font-size: 14px;
+	color:#777
 	&::placeholder {
 		color: #999;
+	}
+	&.focused{
+		width:240px
 	}
 `;
 
