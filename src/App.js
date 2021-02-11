be306f67902354e0d9845ed09284eaf9d106f955
lceihen <2750418124@qq.com>
2021-02-11 10:08:40 +0800

    8-1完成

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import Header from './common/header';
 import store from './store/index'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Header />
+        <BrowserRouter>
+          <Route path='/' exact render={() => <div>home</div>}></Route>
+          <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+        </BrowserRouter>
+
       </Provider>
 
     );
