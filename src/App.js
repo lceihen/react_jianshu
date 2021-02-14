103975beb229f425d360f08776fe02267176a7cd
lceihen <2750418124@qq.com>
2021-02-14 22:59:00 +0800

    修复中

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Header from './common/header';
+import store from './store/index'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Detail from './pages/detail/loadable'
+import Home from './pages/home/index'
+import Login from './pages/login/index'
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+
+        <BrowserRouter>
+
+          <Header />
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/detail/:id' exact component={Detail}></Route>
+          <Route path='/login' exact component={Login}></Route>
+        </BrowserRouter>
+
+      </Provider>
+
+    );
+  }
+
+}
+
+
+export default App
